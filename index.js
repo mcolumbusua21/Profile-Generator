b8743ad816e357757e6bd26e78919f9d5c36d0e0
Mark Columbus <mcolumbusua21@gmail.com>
2021-03-24 08:55:01 -0700

    trying

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const childproc = require('child_process');
+const childproc = require("child_process");
 const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
@@ -12,64 +12,149 @@ const inquirer = require("inquirer");
 const teamArray = [];
 
 function questionCallback(role) {
-    console.log(role);
-    if (role === 'Engineer') {
-        return askEngineerQuestions();
-    } else if (role === 'Intern') {
-        return askInternQuestions();
-    } else {
-        console.log(teamArray);
-        const html = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf-8');
+  console.log(role);
+  if (role === "Engineer") {
+    return askEngineerQuestions();
+  } else if (role === "Intern") {
+    return askInternQuestions();
+  } else {
+    console.log(teamArray);
+    const html = fs.readFileSync(
+      path.join(__dirname, "template.html"),
+      "utf-8"
+    );
 
-        const newHtml = html.replace('[teamArray]', JSON.stringify(teamArray));
+    const newHtml = html.replace("[teamArray]", JSON.stringify(teamArray));
 
-        fs.writeFileSync(path.join(__dirname, 'index.html'), newHtml);
-        if (fs.existsSync(path.join(__dirname, 'index.html'))) {
-            childproc.exec('open -a "Google Chrome" index.html');
-        }
-        process.exit(0);
+    fs.writeFileSync(path.join(__dirname, "index.html"), newHtml);
+    if (fs.existsSync(path.join(__dirname, "index.html"))) {
+      childproc.exec('open -a "Google Chrome" index.html');
     }
-}; 
+    process.exit(0);
+  }
+}
 
 const askManagerQuestions = () => {
-    const managerQuestions = [
-        {
-          type: "input",
-          message: "Please enter the name of the manager:",
-          name: "name",
-        },
-        {
-          type: "input",
-          message: "Please enter the managers employee ID number:",
-          name: "id",
-        },
-        {
-          type: "input",
-          message: "Please enter the managers email address:",
-          name: "email",
-        },
-        {
-          type: "input",
-          message: "Please enter the office number:",
-          name: "office number",
-        },
-      ];
+  const managerQuestions = [
+    {
+      type: "input",
+      message: "Please enter the name of the manager:",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Please enter the managers employee ID number:",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Please enter the managers email address:",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Please enter the office number:",
+      name: "office number",
+    },
+  ];
 
-    return inquirer.prompt(managerQuestions)
-        .then((answers) => {
+  return inquirer.prompt(managerQuestions).then((answers) => {
+    teamArray.push({
+      ...answers,
+      role: "Manager",
+    });
+    function renderPage(teamArray) {
+      let employees = "";
+      for (let i = 0; i < teamArray.length; i++) {
+        if (teamArray[i].role === "Manager") {
+          employees += `
+                        <div class= "card employee-card" id="Manager">
+                            <div class= "card-header">
+                                <h2 class= "card-title">${teamArray[i].name}</h2>
+                                <h3 class= "card-title"><i class="fas fa-mug-hot mr-2"></i>${teamArray[i].role}</h3>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li class="list-group-item"> ID: ${teamArray[i].id}</li>
+                                    <li class="list-group-item"> Office Number: ${teamArray[i].officeNumber}</li>
+                                    <li class="list-group-item"> Email: <a href="mailto:${teamArray[i].email}">${team[i].email}</a></li>
+                                </ul>
+                            </div>
+                        </div>`;
+        } else if (teamArray[i].role === "Engineer") {
+          emlpoyees += `
+                        <div class= "card employee-card" id="Engineer">
+                            <div class= "card-header">
+                                <h2 class= "card-title">${teamArray[i].name}</h2>
+                                <h3 class= "card-title"><i class="fas fa-mug-hot mr-2"></i>${team[i].role}</h3>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li class="list-group-item"> ID: ${teamArray[i].id}</li>
+                                    <li class="list-group-item"> Email: <a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a></li>
+                                    <li class="list-group-item"> GitHub: <a href="http://github.com/:${teamArray[i].github}"> target="_blank">${teamArray[i].github}</a></li>
+                                </ul>
+                            </div>
+                        </div>`;
+          emlpoyees += `
+                        <div class= "card employee-card" id="Engineer">
+                            <div class= "card-header">
+                                <h2 class= "card-title">${teamArray[i].name}</h2>
+                                <h3 class= "card-title"><i class="fas fa-mug-hot mr-2"></i>${teamArray[i].role}</h3>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li class="list-group-item"> ID: ${teamArray[i].id}</li>
+                                    <li class="list-group-item"> Email: <a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a></li>
+                                    <li class="list-group-item"> GitHub: <a href="http://github.com/:${teamArray[i].github}"> target="_blank">${teamArray[i].github}</a></li>
+                                </ul>
+                            </div>
+                        </div>`;
+        } else {
+          emlpoyees += `
+                    <div class= "card employee-card" id="Intern">
+                        <div class= "card-header">
+                            <h2 class= "card-title">${teamArray[i].name}</h2>
+                            <h3 class= "card-title"><i class="fas fa-mug-hot mr-2"></i>${teamArray[i].role}</h3>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item"> ID: ${teamArray[i].id}</li>
+                                <li class="list-group-item"> Email: <a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a></li>
+                                <li class="list-group-item"> School: ${teamArray[i].school}</li>
+                            </ul>
+                        </div>
+                    </div>`;
+          let = `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <script src="script.js"></script>
+                <link rel="stylesheet" href="./style.css">
+                <title>Profile Generator</title>
+            </head>
+            <body>
+                
+            </body>
+            </html>
+            `;
+        }
+      }
+    }
 
-            teamArray.push(new Manager(
-                ...answers);
-
-            return inquirer.prompt({
-                type: "list",
-                message: "Which type of team member would you like to add?",
-                choices: ["Engineer", "Intern", "No more members"],
-                name: "role",
-              }).then(({role}) => questionCallback(role));
-        );
-
-        };
+    return inquirer
+      .prompt({
+        type: "list",
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "No more members"],
+        name: "role",
+      })
+      .then(({ role }) => questionCallback(role));
+  });
+};
 
 const askEngineerQuestions = () => {
   return inquirer
@@ -101,113 +186,55 @@ const askEngineerQuestions = () => {
         choices: ["Engineer", "Intern", "No more members"],
       },
     ])
-    .then(({ role, ...data}) => {
-        teamArray.push({
-            ...data,
-            role: 'Engineer'
-        });
-        return questionCallback(role);
+    .then(({ role, ...data }) => {
+      teamArray.push({
+        ...data,
+        role: "Engineer",
+      });
+      return questionCallback(role);
     });
 };
 
 const askInternQuestions = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      message: "Please enter the name of the intern:",
-      name: "name",
-    },
-    {
-      type: "input",
-      message: "Please enter the interns employee ID number:",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "Please enter the interns email address:",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "Please enter the name of the school the intern is attending:",
-      name: "school",
-    },
-    {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please enter the name of the intern:",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Please enter the interns employee ID number:",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Please enter the interns email address:",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Please enter the name of the school the intern is attending:",
+        name: "school",
+      },
+      {
         type: "list",
         message: "Which type of team member would you like to add?",
         name: "role",
         choices: ["Engineer", "Intern", "No more members"],
       },
-  ])
-  .then(({role, ...data}) => {
+    ])
+    .then(({ role, ...data }) => {
       teamArray.push({
-          ...data,
-          role: 'Intern'
-      })
+        ...data,
+        role: "Intern",
+      });
       return questionCallback(role);
-  });
-};
-
-// const overallQuestions = [
-//   function () {
-//     return {
-//       type: "list",
-//       message: "Which type of team member would you like to add?",
-//       name: "member",
-//       choices: ["Engineer", "Intern", "No more members"],
-//     };
-//   },
-// ];
-
-/*
-function askQuestions() {
-  inquirer
-    .prompt(managerQuestions)
-    .then((data) => {
-      console.log(".then --->", data);
-      const manager = new Manager(
-        data.name,
-        data.id,
-        data.email,
-        data.officeNumber
-      );
-      teamArray.push(manager);
-      console.log("TEAM ARRAY---> ", teamArray);
-      // return askOverallQuestions()
-    })
-    .then(() => {
-      inquirer
-        .prompt({
-          type: "list",
-          message: "Which type of team member would you like to add?",
-          choices: ["Engineer", "Intern", "No more members"],
-          name: "title",
-        })
-        .then((answer) => {
-          switch (answer.title) {
-            case "Engineer":
-              askEngineerQuestions();
-                const engineer = new Engineer;
-              break;
-
-            case "Intern":
-              inquirer
-                askInternQuestions()
-                const intern = new Intern;
-                // .then((data) => console.log("intern data", data));
-              break;
-            default:
-              console.log("No more stuff");
-              break;
-          };
-
-        //   console.log("SECOND QUESTION--->", answer);
-        });
     });
 };
-*/
 
 askManagerQuestions()
-    .then(() => askInternQuestions())
-    .then(() => askQuestions())
-    .then(() => renderPage());
+  .then(() => askInternQuestions())
+  .then(() => askQuestions())
+  .then(() => renderPage());
